473656a2977514f2183a0e9c20ddb0e199a494bc
daicoden <daicoden@copypastel.com>
2009-09-16 18:48:34 -0600

    Updated the config and how the ppp loads it.

--- server/ppp-server.rb
+++ server/ppp-server.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby -rubygems
 %w(yaml haml sequel sinatra).each { |lib| require lib }
 
-CFG = YAML.load_file(File.dirname(__FILE__) + '/config.yml')
+CFG = YAML.load_file( YAML.load_file(File.dirname(__FILE__) + '/config.yml')[:project_config] )
 
 #-----------------------------
 # DB setup
@@ -10,6 +10,7 @@ DB = Sequel.connect('sqlite://ppp-server.db')
 
 DB.create_table :commands do
   String :command, :primary_key => true
+  String :path
 end unless DB.table_exists?(:commands)
 #-----------------------------
 # Models
@@ -20,28 +21,51 @@ end
 # URI Processing
 #-----------------------------
 get '/' do
-  @records = DB[:commands].all
+  @commands = Command.all
   haml :index
 end
 
 post '/' do
   DB_refresh()
-  @records = DB[:commands].all
+  @commands = Command.all
   haml :index
 end
+
+post '/run' do
+  params[:commands].split(';').each do |command|
+    record = Command[:command => command]
+    # Return 501 (Not Implemented) if command not found
+    status 501 and return command if execute.nil?
+    # Return 500 (Server Error) if command does not exit with 0
+    status 500 and return command unless system(record.path + record.command)
+  end
+
+  status 200
+  'true'
+end
+
+get '/run' do
+  record = Command[:command => 'gen-preambles']
+  #result = %x["#{record.path + record.command}"]
+  #p result
+  status 500 and return 'Error on gen-preambles' unless system(record.path + record.command)
+  status 200 and return 'Ran gen-preambles'
+end
+
 #-----------------------------
 # Helper Funcions
 #-----------------------------
 # Add any new scripts to database
 def DB_refresh
-  # Each Script File (sf)
-  CFG[:file_structure][:scripts].each do |sf|
+  # Each Script Location (sloc)
+  CFG[:file_structure][:scripts].each do |sloc|
+    sloc = CFG[:file_structure][:root]+sloc
     # Open the directory and add any scripts to executable list
-    Dir.open(CFG[:file_structure][:root]+sf) do |dir|
-      # Loop through each File (f) in the Script File (sf) location
-      dir.each do |f|
-        next if f == '.' or f == '..' or DB[:commands][:command => f]
-        DB[:commands].insert(:command => f)
+    Dir.open(sloc) do |dir|
+      # Loop through each Script File (sfile) in the Script Location (sloc) location
+      dir.each do |sfile|
+        next if sfile == '.' or sfile == '..' or Command[:command => sfile]
+        Command.insert(:command => sfile, :path => sloc)
       end
     end
   end
@@ -55,24 +79,12 @@ __END__
 !!!
 %html
   %head
-    %title Add Commands
+    %title Script List
   %body
     Records Found:
     %br
-    - @records.each do |record|
-      = record[:command]
+    - @commands.each do |command|
+      = command.command
       %br
     %form{ :action => '/', :method => :post}
       %input{ :type =>'submit' :value => 'Refresh'}
-
-@@push
-!!!
-%html
-  %head
-    %title
-  %body
-    Records Found:
-    %br
-    - @records.each do |record|
-      = record[:command]
-      %br
